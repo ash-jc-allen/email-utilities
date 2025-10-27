03bec2c9d64bac00086912300df920969528de56
Ash Allen <mail@ashallendesign.co.uk>
2025-10-27 16:05:01 +0000

    Fix PHPStan issues.

--- src/Lists/DisposableDomainList.php
+++ src/Lists/DisposableDomainList.php
@@ -10,13 +10,14 @@ use Illuminate\Support\Facades\File;
 class DisposableDomainList
 {
     /**
-     * @return string[]
+     * @return list<string>
      * @throws FileNotFoundException
      */
     public static function get(): array
     {
+        /** @var string $listLocation */
         $listLocation = config('email-utilities.disposable_email_list_path') ?: __DIR__.'/../../lists/disposable-domains.json';
 
-        return File::json($listLocation);
+        return array_values(File::json($listLocation));
     }
 }
