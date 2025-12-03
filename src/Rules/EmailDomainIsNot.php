f1d16b6b8ff69e82a0917857f37ab72ee862e986
Ash Allen <mail@ashallendesign.co.uk>
2025-12-03 12:05:58 +0000

    Added named parameter back.

--- src/Rules/EmailDomainIsNot.php
+++ src/Rules/EmailDomainIsNot.php
@@ -35,6 +35,6 @@ class EmailDomainIsNot implements ValidationRule
 
     public static function disposable(): self
     {
-        return new self(DisposableDomainList::get());
+        return new self(patterns: DisposableDomainList::get());
     }
 }
